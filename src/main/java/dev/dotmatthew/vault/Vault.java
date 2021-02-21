6f1feb7f7581233b1e917409894466fa5f4a3137
Mathias Dollenbacher <hello@mdollenbacher.net>
2021-02-21 19:27:36 +0100

    added VaultResponse and changed Http Client (OkHttpClient)

--- src/main/java/dev/dotmatthew/vault/Vault.java
+++ src/main/java/dev/dotmatthew/vault/Vault.java
@@ -1,7 +1,20 @@
 package dev.dotmatthew.vault;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import dev.dotmatthew.vault.exceptions.VaultJsonException;
+import dev.dotmatthew.vault.exceptions.VaultResponseCodeException;
+import dev.dotmatthew.vault.response.VaultResponse;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
 /**
- * Created by Mathias Dollenbacher on 21.02.2021.
+ * Created by Mathias Dollenbacher on 21.02.21.
  *
  * <p>
  *
@@ -9,6 +22,34 @@ package dev.dotmatthew.vault;
  */
 public class Vault {
 
+    private final OkHttpClient client;
 
+    private final String vaultServer;
+    private final String vaultToken;
+
+    public Vault(@NotNull final String vaultServer,@NotNull final String vaultToken) {
+        this.vaultServer = vaultServer;
+        this.vaultToken = vaultToken;
+        this.client = new OkHttpClient();
+    }
+
+    public VaultResponse readPath(@NotNull String path) {
+        final Request request = new Request.Builder()
+                .url(vaultServer+"/v1/"+path)
+                .addHeader("X-Vault-Token", this.vaultToken)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+
+            if(response.code() != 200) {
+                throw new VaultResponseCodeException("The response was not succesful (Code: " + response.code()+")!");
+            }
+
+            //final String jsonResponse = Objects.requireNonNull(response.body()).string();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ src/main/java/dev/dotmatthew/vault/exceptions/VaultJsonException.java
@@ -0,0 +1,16 @@
+package dev.dotmatthew.vault.exceptions;
+
+/**
+ * Created by Mathias Dollenbacher on 21.02.21.
+ *
+ * <p>
+ *
+ * @author Mathias Dollenbacher
+ */
+public class VaultJsonException extends RuntimeException {
+
+    public VaultJsonException(final String message) {
+        super(message);
+    }
+
+}
